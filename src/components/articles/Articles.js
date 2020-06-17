6c6854d684df4938819523ad6fde72b49870d7fd
christian dungca <cdungca14@gmail.com>
2020-06-17 19:02:18 +0100

    changed position for navigation btn. Changed the layout of articles to be in a grid of 2 columns by however many. I still need to style the text container for the articles and change the proportions of the grid area. Next time I also need to fix the layout so that there are no overlapping of the different sections.

--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -7,22 +7,25 @@ const Articles = () => {
     <div className="Articles">
       <div className="articles-container">
         {allArticles.map((article) => {
-          const backgroundImage = {
+          const articleStyle = {
             backgroundImage: `url(${article.imageUrl})`,
             backgroundPosition: `${article.imagePosition}`,
-          };
+            backgroundSize: 'cover'
+          }; 
+
+          const articleShortDescription = article.content.toString().substring(0, 80);
 
           return (
-            <div className="article__image" style={backgroundImage}>
-              {article.titleColor === "white" ? (
-                <h1 className="article__heading article__heading--white">
-                  {article.title}
-                </h1>
-              ) : (
-                <h1 className="article__heading article__heading--black">
-                  {article.title}
-                </h1>
-              )}
+            <div className="article-container"> 
+              <div className="article__img" style={articleStyle}></div>
+              <div className="article-text-container">
+                <h3 className="article-text-container__title">
+                  {article.title}-
+                </h3>
+                <p className="article-text-container__description">
+                  {articleShortDescription}...
+                </p>
+              </div>
             </div>
           );
         })}
