1b403d4c35fb6c544a8981277f6119456a3a0b8b
Christan-Dungca <cdungca14@gmail.com>
2020-07-17 16:17:31 +0100

    created a horizontal scrolling component for articles section. Function component handles several different mouse events to keep track of scroll positon

--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -1,51 +1,81 @@
-import React from "react";
+import React, { Fragment, useRef } from "react";
 import allArticles from "./allArticles";
 import "./Articles.scss";
 
 const Articles = () => {
-  let articleNumber = 0;
-  return (
-    <React.Fragment>
-      <div className="Articles">
-        {/* <div className="Articles__article box--1"></div>
-      <div className="Articles__article box--2"></div>
-      <div className="Articles__article box--3"></div>
-    <div className="Articles__article box--4"></div> */}
+  // Variables for slider
+  const sliderRef = useRef(null);
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-        {allArticles.map((article) => {
-          // counter for dynamically adding classes names
-          articleNumber += 1;
-          // styling for each box
-          let boxStyles = {
-            backgroundImage: `url(${article.imageUrl})`,
-            backgroundPosition: `${article.imagePosition}`,
-            backgroundSize: "cover",
-          };
-          // returns box representing each article
-          return (
-            <div className={`box box--${articleNumber}`} style={boxStyles}>
-              <div className="box__overlay">
-                <div className="box__title">{article.title}</div>
-                {/* <div className="box__description">Deciding on you travel date can be the hardest thing! Everyone wants travel during that perfect time. We're here to help! </div> */}
-              </div>
+  const handleMouseDown = (e) => {
+    const slider = sliderRef.current;
+
+    slider.classList.add("active");
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+
+    console.log(slider, isDown, startX, scrollLeft);
+  };
+
+  const handleMouseUpLeave = (e) => {
+    const slider = sliderRef.current;
+    isDown = false;
+    slider.classList.remove("active");
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDown) return;
+    e.preventDefault(); 
+
+    const slider = sliderRef.current;
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+  }
+
+  return (
+    <Fragment>
+      <div className="Articles">
+        <div
+          className="slider"
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUpLeave}
+          onMouseMove={handleMouseMove}
+          ref={sliderRef}
+        >
+          <div className="article-box">
+            <div className="article-box__overlay">
+              <div className="article-box__title"> When is the best time to visit Japan?</div>
+              <div className="article-box__description"> Each season will add different uniqueness to your trip. Learn what time of the year is best for you visit.</div>
             </div>
-          );
-        })}
+          </div>
+          <div className="article-box">
+            <div className="article-box__overlay">
+              <div className="article-box__title"> When is the best time to visit Japan?</div>
+              <div className="article-box__description"> Each season will add different uniqueness to your trip. Learn what time of the year is best for you visit.</div>
+            </div>
+          </div>
+          <div className="article-box">
+            <div className="article-box__overlay">
+              <div className="article-box__title"> When is the best time to visit Japan?</div>
+              <div className="article-box__description"> Each season will add different uniqueness to your trip. Learn what time of the year is best for you visit.</div>
+            </div>
+          </div>
+          <div className="article-box">
+            <div className="article-box__overlay">
+              <div className="article-box__title"> When is the best time to visit Japan?</div>
+              <div className="article-box__description"> Each season will add different uniqueness to your trip. Learn what time of the year is best for you visit.</div>
+            </div>
+          </div>
+
+        </div>
       </div>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
 export default Articles;
-
-{
-  /* 
-
-  //? code that renders each article from all articles file
-  
-  <div className="Articles">
-  {allArticles.map((article) => {
-    return <Article title={article.title} content={article.content} />;
-  })}
-</div> */
-}
